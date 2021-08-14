6c55cb41e8968aa13def1e4d89e274d1b51cc6d7
Vítor <vrgermano159@gmail.com>
2021-08-13 22:29:02 -0300

    função passagem

--- JS/inicio_bruxo.js
+++ JS/inicio_bruxo.js
@@ -1,12 +1,17 @@
-while (true) {
-    var escolha = prompt("Escolha A<br> Escolha B<br> Qual sua escolha?")
-    if (escolha.toLowerCase() == "a") {
-        document.write("Escolha correta!")
-        break;
-    } else if (escolha.toLowerCase() == "b") {
-        document.write("Escolha errada!")
-        break;
-    } else {
-        alert("Insira uma opção válida")
+function clicouBotao() {
+    while (true) {
+        var escolha = prompt("Escolha A \nEscolha B \nQual sua escolha?")
+        if (escolha.toLowerCase() == "a") {
+            console.log("Escolha correta!")
+            part1.style.display = "none";
+            part2_boa.style.display = "block";
+            break;
+        } else if (escolha.toLowerCase() == "b") {
+            console.log("Escolha errada!")
+            part1.style.display = "none";
+            part2_ruim.style.display = "block";
+            break;
+        } else {
+            alert("Insira uma opção válida")
+        }
     }
-}
